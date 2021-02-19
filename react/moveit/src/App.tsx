deebe5eb648494c9b7f129498fc4c69ebf87bd37
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2021-02-19 18:43:06 -0300

    02: Desvendando o Next.js

---
+++ react/moveit/src/App.tsx
@@ -0,0 +1,10 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css'
+
+export function App() {
+  return ( 
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
